be777c7704359b03f07a9eb8700d87230ddbc375
Anita Chiele <anita.lps@gmail.com>
2019-03-01 04:31:36 -0300

    Flags [S02A06]
    Correção do posicionamento do comentário.

--- capitulo01/flags.js
+++ capitulo01/flags.js
@@ -5,8 +5,8 @@
     i - ignora a diferença de maiúscula e minúscula.
  */
 
-// em javascript tudo que estiver dentro de duas barras está a expressão regular.
 const texto = 'Carlos assinou o abaixo-assinado.'
+// em javascript tudo que estiver dentro de duas barras está a expressão regular.
 console.log('C ou ab: '); console.log(texto.match(/C|ab/));
 console.log('c ou ab [i]: '); console.log(texto.match(/c|ab/i));
 console.log('c ou ab: '); console.log(texto.match(/c|ab/));
