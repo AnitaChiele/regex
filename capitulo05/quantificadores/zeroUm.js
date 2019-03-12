2b965ee7aeb89fa4ea5723d1ede0a0c21cffe156
Anita Chiele <anita.lps@gmail.com>
2019-03-12 01:09:06 -0300

    Quantificador + [S05A34]

--- capitulo05/quantificadores/zeroUm.js
+++ capitulo05/quantificadores/zeroUm.js
@@ -2,7 +2,7 @@ console.log("/****************************************/")
 console.log("/***             zeroUm.js            ***/")
 console.log("/****************************************/\n")
 
-const t1 = 'De longe eu avistei o fogo e uma pessoa gritando: FOGOOOOOO'
+const t1 = 'De longe eu avistei o fogo e uma pessoa gritando: FOGOOOOOO!'
 const t2 = 'There is a big fog in NYC'
 
 // ? -> zero ou um (opcional)
